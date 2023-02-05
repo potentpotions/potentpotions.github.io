3d8d370e80de0d6724a20bfe2c60b622ff662add
PrizemArtNFT <116934352+PrizemArtNFT@users.noreply.github.com>
2023-02-05 15:29:52 -0800

    under construction page, logo, mega font, centered layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,24 @@
 import Link from 'next/link'
-
+import style from './index.module.scss'
 export default function Home() {
+  /**
+   * construction page
+   * need custom font setup
+   * need logo
+   * need background pattern
+   * need copy 
+   */
   return (
-    <div>
-      Hello World. <Link href="/about">About</Link>
+    <div className={style.parent}>
+      <div className={style.content}>
+        <img src="logo/logo.webp" style={{maxWidth:'60vw', minWidth: '20vw'}}/>
+        <br/>
+        We are currently under construction...
+        <br />
+        See you soon!
+        {/* Hello World. <Link href="/about">About</Link> */}
+      </div>
     </div>
+
   )
 }
