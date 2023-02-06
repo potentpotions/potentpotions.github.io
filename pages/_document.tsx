f11e29ce42c11b97480d6904014d328bdf0dbdb5
PrizemArtNFT <116934352+PrizemArtNFT@users.noreply.github.com>
2023-02-05 16:14:15 -0800

    fixing SEO missing description tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head title='Potent Potions World 📚 NFT Comic-book'/>
+      <Head>
+        <title>Potent Potions NFT Comicbook 📚</title>
+        <meta name="description" content="Discover unique and collectible NFT comicbook featuring stunning illustrations and original storylines. Own a piece of digital art and join the future of art collecting with our NFT comicbooks." />
+        </Head>
+     
       <body style={{padding:0, margin:0}}>
         <Main />
         <NextScript />
